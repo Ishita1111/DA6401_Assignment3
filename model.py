a6e6d32e5e6612e7c1b1481901f0dc34a385a09e
Ishita Mittal <ishitamittal@gmail.com>
2026-05-12 12:12:13 +0530

    Transformer constructor

--- model.py
+++ model.py
@@ -488,13 +488,13 @@ class Transformer(nn.Module):
 
     def __init__(
         self,
-        src_vocab_size: int,
-        tgt_vocab_size: int,
-        d_model:   int   = 512,
-        N:         int   = 6,
-        num_heads: int   = 8,
-        d_ff:      int   = 2048,
-        dropout:   float = 0.1,
+        src_vocab_size: int = 10000,
+        tgt_vocab_size: int = 10000,
+        d_model: int = 512,
+        N: int = 6,
+        num_heads: int = 8,
+        d_ff: int = 2048,
+        dropout: float = 0.1,
         checkpoint_path: str = None,
     ) -> None:
         super().__init__()
